469e823731460be129d37ae1ebc035bcfc488a8d
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-03-30 16:57:25 +0200

    prettify code and update doc

--- test/test_krdem2.f90
+++ test/test_krdem2.f90
@@ -37,7 +37,7 @@ contains
 
    end subroutine f
 
-   subroutine jac(t,y,yprime,pd,cj,rpar,ipar)
+   subroutine jac(t, y, yprime, pd, cj, rpar, ipar)
    !! Jacobian routine.
       real(wp), intent(in) :: t
       real(wp), intent(in) :: y(neq)
@@ -46,21 +46,21 @@ contains
       real(wp), intent(in) :: cj
       real(wp), intent(in):: rpar
       integer, intent(in) :: ipar
-        
-      ! First define the Jacobian matrix for the right-hand side of the ODE: 
+
+      ! First define the Jacobian matrix for the right-hand side of the ODE:
       ! Y' = F(T,Y) , i.e. dF/dY.
-      pd(1,1) = zero
-      pd(1,2) = one
-      pd(2,1) = -200*y(1)*y(2) - one
-      pd(2,2) = 100*(one - y(1)**2)
-      
-      ! Next update the Jacobian with the right-hand side to form the DAE Jacobian: 
+      pd(1, 1) = zero
+      pd(1, 2) = one
+      pd(2, 1) = -200*y(1)*y(2) - one
+      pd(2, 2) = 100*(one - y(1)**2)
+
+      ! Next update the Jacobian with the right-hand side to form the DAE Jacobian:
       ! CJ*dR/dY' + dR/dY = CJ*I - dF/dY.
-      pd(1,1) = cj-pd(1,1)
-      pd(1,2) = -pd(1,2)
-      pd(2,1) = -pd(2,1)
-      pd(2,2) = cj-pd(2,2)
-      
+      pd(1, 1) = cj - pd(1, 1)
+      pd(1, 2) = -pd(1, 2)
+      pd(2, 1) = -pd(2, 1)
+      pd(2, 2) = cj - pd(2, 2)
+
    end subroutine jac
 
    pure subroutine rt(neq, t, y, yp, nrt, rval, rpar, ipar)
@@ -81,21 +81,21 @@ contains
 end module krdem2_module
 
 program test_krdem2
-!! Test program for `daskr`: nonstiff problem.
+!! Test program for `daskr`: intermittently stiff problem.
 !!
 !! The initial value problem is:
 !!```
-!!    dy/dt = ((2*log(y) + 8)/t - 5)*y,  y(1) = 1,  1 <= t <= 6
-!!``` 
-!! The solution is:
+!!    dy1/dt = y2
+!!    dy2/dt = 100*(1 - y1**2)*y2 - y1,
+!!
+!!    y1(0) = 2,  y2(0) = 0,  0 <= t <= 200
+!!    y1'(0) = 0, y2'(0) = -2
 !!```
-!!    y(t) = exp(-t**2 + 5*t - 4), y'(1) = 3
+!! The root function is:
 !!```
-!! The two root functions are:
+!!    r1(t, y, y') = y1
 !!```
-!!    r1(t,y,y') = dy/dt  (with root at t = 2.5),
-!!    r2(t,y,y') = log(y) - 2.2491  (with roots at t = 2.47 and 2.53)
-!!```
+!! The analytical solution is not known, but the zeros of `y1` are known to 15 figures.
 !!
 !! If the errors are too large, or other difficulty occurs, a warning message is printed.
 !! To run the demonstration problem with full printing, set `kprint=3`.
@@ -117,7 +117,7 @@ program test_krdem2
    idid = 0
 
    ! Set tolerance parameters and print heading.
-   ! Note that INFO(2) is set to 1, indicating that RTOL and ATOL are arrays. Each entry of 
+   ! Note that INFO(2) is set to 1, indicating that RTOL and ATOL are arrays. Each entry of
    ! RTOL and ATOL must then be defined.
    info = 0
    info(2) = 1
@@ -137,41 +137,41 @@ program test_krdem2
    ! Note: JTYPE indicates the Jacobian type:
    ! JTYPE = 1 ==> Jacobian is dense and user-supplied
    ! JTYPE = 2 ==> Jacobian is dense and computed internally
-   
+
    ! Loop over JTYPE = 1, 2.  Set remaining parameters and print JTYPE.
-   do jtype = 1,2
-      
+   do jtype = 1, 2
+
       ! Set INFO(1) = 0 to indicate start of a new problem
       ! Set INFO(5) = 2-JTYPE to tell DDASKR the Jacobian type.
       info(1) = 0
       info(1) = 0
-      info(5) = 2-jtype
+      info(5) = 2 - jtype
       t = zero
       y(1) = two
       y(2) = zero
       yprime(1) = zero
       yprime(2) = -two
       tout = 20.0_wp
-      
+
       if (kprint > 2) then
          write (lun, '(/, 70("."), /, a, i2, /)') "Solution with JTYPE =", jtype
       end if
-      
+
       ! Call DDASKR in loop over TOUT values = 20, 40, ..., 200.
       do iout = 1, 10
-      
+
          do
-            call ddaskr(res,neq,t,y,yprime,tout, info,rtol,atol,idid,  &
-                              rwork,lrw,iwork,liw,rpar,ipar,jac,psdum, rt,nrt,jroot)
-            
+            call ddaskr(res, neq, t, y, yprime, tout, info, rtol, atol, idid, &
+                        rwork, lrw, iwork, liw, rpar, ipar, jac, psdum, rt, nrt, jroot)
+
             ! Print Y1 and Y2.
             if (kprint > 2) then
                write (lun, '(a, e15.7, 5x, a, e15.7, 5x, a, e15.7)') &
                   "At t =", t, "y1 =", y(1), "y2 =", y(2)
             end if
-            
+
             if (idid < 0) exit
-            
+
             ! If no root found, increment TOUT and loop back.
             ! If a root was found, write results and check root location.
             ! Then return to DDASKR to continue the integration.
@@ -179,14 +179,14 @@ program test_krdem2
                tout = tout + 20.0_wp
                exit
             else
-          
+
                if (kprint > 2) then
                   write (lun, '(/, a, e15.7, 2x, a, i3)') "Root found at t =", t, "JROOT =", jroot(1)
-               end if                 
-               
+               end if
+
                kroot = int(t/81.2_wp + 0.5_wp)
                tzero = 81.17237787055_wp + float(kroot - 1)*81.41853556212_wp
-               errt = t - tzero            
+               errt = t - tzero
                if (kprint > 2) then
                   write (lun, '(a, e12.4, /)') "Error in t location of root is", errt
                end if
@@ -195,17 +195,17 @@ program test_krdem2
                   nerr = nerr + 1
                   if (kprint >= 2) then
                      write (lun, '(/, a, /)') 'WARNING: Root error exceeds 1.0'
-                  end if 
+                  end if
                end if
-                                 
+
             end if
-            
+
          end do
 
          if (idid < 0) exit
-         
+
       end do
-   
+
       ! Problem complete. Print final statistics.
       if (idid < 0) nerr = nerr + 1
       nst = iwork(11)
@@ -214,8 +214,8 @@ program test_krdem2
       nrte = iwork(36)
       nrea = nre
       if (jtype == 2) nre = nre + neq*nje
-      
-      if (kprint >= 2 ) then
+
+      if (kprint >= 2) then
          write (lun, '(/, a)') 'Final statistics for this run:'
          write (lun, '(a, i5)') 'number of steps =', nst
          write (lun, '(a, i5)') 'number of Gs    =', nre
@@ -223,9 +223,9 @@ program test_krdem2
          write (lun, '(a, i5)') 'number of Js    =', nje
          write (lun, '(a, i5)') 'number of Rs    =', nrte
       end if
-   
+
    end do
-    
+
    if (kprint >= 1) then
       write (lun, '(/, a, i3)') 'Number of errors encountered =', nerr
    end if
